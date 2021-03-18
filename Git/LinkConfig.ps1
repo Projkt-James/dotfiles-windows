1cf3ab38ba271d3ad86ca6a4309161c87969fb61
James Hanford <james@projkt.io>
2021-03-18 23:19:03 +1000

    Added gitconfig dotfile and symlink script

---
+++ Git/LinkConfig.ps1
@@ -0,0 +1,14 @@
+# Create symlink to .gitconfig in home directory
+
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+
+if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "Removing any existing gitconfig from home path" -ForegroundColor Gray
+    Remove-Item "$HOME/.gitconfig"
+
+    Write-Host "Creating symlink to .gitconfig file!" -ForegroundColor Gray
+    New-Item -ItemType symboliclink -Path $HOME -Name .gitconfig -Value "$PWD\.gitconfig" | Out-Null
+    Write-Host "Complete!" -ForegroundColor Green
+} else {
+    Write-Host "[Error] Unable to create symlink, Admin privileges required!" -ForegroundColor DarkRed
+}
